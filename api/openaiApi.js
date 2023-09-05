a1a22abda759d83b276824505457a6429ddb46c5
kylestocks <kjd3299@gmail.com>
2023-09-05 00:21:57 -0700

    test

--- api/openaiApi.js
+++ api/openaiApi.js
@@ -1,13 +1,44 @@
 import dotenv from "dotenv";
-import OpenAI from 'openai';
+import { Client, GatewayIntentBits } from "discord.js"; // Import necessary classes
+import OpenAI from 'openai'; // Import OpenAI
 
 dotenv.config();
 
+// Initialize the Discord.js client with the appropriate intents
+const intents = new GatewayIntentBits([
+  "GUILDS",
+  "GUILD_MESSAGES",
+  // Add other intents you need here
+]);
+
+const client = new Client({ intents });
+
+// Initialize OpenAI with your API key
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: process.env.OPENAI_API_KEY // Replace with your API key
 });
 
-export async function getAnswer(question) {
+// Event listener when the bot is ready
+client.once("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
+});
+
+// Event listener when the bot receives a message
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) return; // Ignore messages from other bots
+
+  if (message.content.startsWith("!ask")) {
+    const question = message.content.slice(5); // Remove the "!ask" prefix
+    const answer = await getAnswer(question);
+    message.channel.send(answer);
+  }
+
+  // Add more command handlers or event listeners here
+
+});
+
+// Function to get an answer from OpenAI
+async function getAnswer(question) {
   try {
     const chatResponse = await openai.chat.create({
       model: "gpt-4",
@@ -28,23 +59,5 @@ export async function getAnswer(question) {
   }
 }
 
-export async function getLessonPlan(topic) {
-  try {
-    const chatResponse = await openai.chat.create({
-      model: "gpt-4",
-      messages: [
-        { role: "system", content: "You are a helpful assistant specialized in creating lesson plans." },
-        { role: "user", content: `Create a preschool lesson plan about ${topic}.` }
-      ],
-    });
-
-    if (chatResponse.status !== 200) {
-      return "Sorry, I can't generate that lesson plan.";
-    }
-
-    return chatResponse.data.choices[0]?.message?.content || "No lesson plan available.";
-  } catch (error) {
-    console.error("Error in getLessonPlan:", error);
-    return "Sorry, an error occurred while generating the lesson plan.";
-  }
-}
+// Login to Discord using the provided bot token
+client.login(process.env.BOT_TOKEN);
