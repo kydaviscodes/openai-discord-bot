e7aef2c627859855102303ac3b91b53dc2580a55
kylestocks <kjd3299@gmail.com>
2023-09-05 00:18:03 -0700

    test

--- api/openaiApi.js
+++ api/openaiApi.js
@@ -1,11 +1,11 @@
 import dotenv from "dotenv";
 import OpenAI from 'openai';
 
+dotenv.config();
+
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
 });
-
-dotenv.config();
 
 export async function getAnswer(question) {
   try {
