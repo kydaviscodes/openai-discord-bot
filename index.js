62fa451ea8b0bd1d506df32d26f31b73aef262db
kylestocks <kjd3299@gmail.com>
2023-09-05 00:59:14 -0700

    intents

--- index.js
+++ index.js
@@ -1,44 +1,60 @@
-import Discord from "discord.js";
+import Discord, { GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
 import { openaiAnswer, generateLessonPlan } from "./helpers.js";
 
-
 dotenv.config();
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES] });
+
+const client = new Discord.Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        // Add other intents as needed
+    ]
+});
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`)
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("reconnecting", () => {
-  console.log(`Reconnecting - ${client.user.tag}!`)
+    console.log(`Reconnecting - ${client.user.tag}!`);
 });
 
 client.on("disconnect", () => {
-  console.log(`Disconnect - ${client.user.tag}!`)
+    console.log(`Disconnect - ${client.user.tag}!`);
 });
 
-client.on("message", async message => {
-  try {
-  if (message.author.bot) return;
+client.on("message", async (message) => {
+    try {
+        if (message.author.bot) return;
 
-  if (message.content.includes("@here") || message.content.includes("@everyone") || message.type == "REPLY") return;
+        if (
+            message.content.includes("@here") ||
+            message.content.includes("@everyone") ||
+            message.type === "REPLY"
+        )
+            return;
 
-  if (message.content === "ping") {
-    message.reply("pong");
-    return;
-  }
-  if (message.content.startsWith("/")) {
-    if (message.content.startsWith("/lessonplan")) {
-      generateLessonPlan(message, client);
+        if (message.content === "ping") {
+            message.reply("pong");
+            return;
+        }
+
+        if (message.content.startsWith("/")) {
+            if (message.content.startsWith("/lessonplan")) {
+                generateLessonPlan(message, client);
+            }
+        }
+
+        if (
+            message.mentions.has(client.user.id) ||
+            message.content.toString().includes(process.env.ROBOT_USER_ID)
+        ) {
+            openaiAnswer(message, client);
+        }
+    } catch (error) {
+        console.error("Error in message event:", error);
     }
-  }
-  if (message.mentions.has(client.user.id) || message.content.toString().includes(process.env.ROBOT_USER_ID)) {
-    openaiAnswer(message, client);
-  }
-  } catch (error) {
-    console.error('Error in message event:', error);
-  }
 });
 
 client.login(process.env.CLIENT_TOKEN);
