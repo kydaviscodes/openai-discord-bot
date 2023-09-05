ce6bb37955bcf390277a8aba17723502f3b8cf85
kylestocks <kjd3299@gmail.com>
2023-09-05 00:27:01 -0700

    branch

--- api/openaiApi.js
+++ api/openaiApi.js
@@ -1,10 +1,9 @@
 import dotenv from "dotenv";
-import { Client, GatewayIntentBits } from "discord.js"; // Import necessary classes
-import OpenAI from 'openai'; // Import OpenAI
+import { Client, GatewayIntentBits } from "discord.js"; // Correct import
+import OpenAI from 'openai';
 
 dotenv.config();
 
-// Initialize the Discord.js client with the appropriate intents
 const intents = new GatewayIntentBits([
   "GUILDS",
   "GUILD_MESSAGES",
@@ -13,31 +12,26 @@ const intents = new GatewayIntentBits([
 
 const client = new Client({ intents });
 
-// Initialize OpenAI with your API key
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // Replace with your API key
+  apiKey: process.env.OPENAI_API_KEY
 });
 
-// Event listener when the bot is ready
 client.once("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-// Event listener when the bot receives a message
 client.on("messageCreate", async (message) => {
-  if (message.author.bot) return; // Ignore messages from other bots
+  if (message.author.bot) return;
 
   if (message.content.startsWith("!ask")) {
-    const question = message.content.slice(5); // Remove the "!ask" prefix
+    const question = message.content.slice(5);
     const answer = await getAnswer(question);
     message.channel.send(answer);
   }
 
   // Add more command handlers or event listeners here
-
 });
 
-// Function to get an answer from OpenAI
 async function getAnswer(question) {
   try {
     const chatResponse = await openai.chat.create({
@@ -59,5 +53,4 @@ async function getAnswer(question) {
   }
 }
 
-// Login to Discord using the provided bot token
 client.login(process.env.BOT_TOKEN);
