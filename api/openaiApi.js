f912a62a97c69c8560001a17fad70a8805bcf630
kylestocks <kjd3299@gmail.com>
2023-09-05 14:46:39 -0700

    migration

--- api/openaiApi.js
+++ api/openaiApi.js
@@ -1,13 +1,13 @@
-import { Configuration, OpenAIApi } from "openai";
+import OpenAI from "openai";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const configuration = new Configuration({
+
+
+const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY
 });
-
-const openai = new OpenAIApi(configuration);
 
 export async function getAnswer(question) {
   try {
