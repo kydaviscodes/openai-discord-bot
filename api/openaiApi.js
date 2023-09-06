4de5e44ab1aec327cf8ce5ef4c51a81e992eb0cc
kylestocks <kjd3299@gmail.com>
2023-09-05 17:15:01 -0700

    refined lessonplans

--- api/openaiApi.js
+++ api/openaiApi.js
@@ -4,46 +4,67 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+  apiKey: process.env.OPENAI_API_KEY
 });
 
-console.log("OpenAI initialized object:", openai);
-console.log("Is OPENAI_API_KEY defined?", Boolean(process.env.OPENAI_API_KEY));
-console.log("Is chat.completions.create available?", Boolean(openai.chat && openai.chat.completions.create));
+function splitLessonPlan(lessonPlan) {
+  const sections = ["Objective", "Materials", "Introduction", "Activities", "Closure", "Extension Activities"];
+  let result = {};
+  let remainingText = lessonPlan;
 
-export async function getAnswer(question) {
-  console.log("Question received:", question);
-  try {
-    const chatResponse = await openai.chat.completions.create({
-      model: "gpt-3.5-turbo",
-      messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: question }
-      ],
+  sections.forEach((section) => {
+    let startIdx = remainingText.indexOf(section);
+    if (startIdx === -1) {
+      result[section] = "Not provided";
+      return;
+    }
+
+    // Find the start index of the next section
+    let nextStartIdx = remainingText.length;
+    sections.forEach((nextSection) => {
+      let tempIdx = remainingText.indexOf(nextSection, startIdx + section.length);
+      if (tempIdx !== -1 && tempIdx < nextStartIdx) {
+        nextStartIdx = tempIdx;
+      }
     });
 
-    return chatResponse.choices[0]?.message?.content || "No answer available.";
-  } catch (error) {
-    console.error("Error in getAnswer:", error);
-    return "Sorry, I can't answer that question. \n" + error;
-  }
+    // Extract content for the current section
+    let content = remainingText.slice(startIdx + section.length, nextStartIdx).trim();
+    result[section] = content;
+
+    // Update remainingText
+    remainingText = remainingText.slice(nextStartIdx);
+  });
+
+  return result;
 }
 
-export async function getLessonPlan(topic) {
+export async function getLessonPlan(topic, ageGroup) {
   try {
     const chatResponse = await openai.chat.completions.create({
       model: "gpt-3.5-turbo",
       messages: [
-        { role: "system", content: "You are a helpful assistant specialized in creating lesson plans." },
-        { role: "user", content: `Create a preschool lesson plan about ${topic}.` }
+        {
+          "role": "system",
+          "content": "You are a preschool teacher AI named ChatABC. You help preschool teacher create lesson plans based on a provided topic and age group. When you receive a message starting with /lessonplan, you will take the following topic and age group, and formulate a lesson plan. The lesson plans should be broken down into categories. \"Objective\", \"Materials\", \"Introduction\", \"Activities\", \"Closure\", and \"Extension Activities\". Prioritize scaffolding, and give questions the teacher can ask the students along the way."
+        },
+        { "role": "user", "content": "/lessonplan " + topic },
+        { "role": "assistant", "content": `Sure! What is the age group for this lesson plan?` },
+        { "role": "user", "content": ageGroup }
       ],
+      temperature: 1,
+      max_tokens: 256,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0,
     });
 
-    return chatResponse.choices[0]?.message?.content || "No lesson plan available.";
+    const fullLessonPlan = chatResponse.choices[0]?.message?.content || "No lesson plan available.";
+    const parsedLessonPlan = splitLessonPlan(fullLessonPlan);
+    return parsedLessonPlan;
+
   } catch (error) {
     console.error("Error in getLessonPlan:", error);
     return "Sorry, an error occurred while generating the lesson plan.";
   }
 }
-
-export { openai as OpenAI };
--- helpers.js
+++ helpers.js
@@ -17,21 +17,30 @@ export async function openaiAnswer(message, client) {
 
 export async function generateLessonPlan(message, client) {
   try {
-    const topic = message.content.replace("/lessonplan", "").trim();
+    const userInput = message.content.replace("/lessonplan", "").trim().split(" ");
+    const topic = userInput[0];
+    const ageGroup = userInput.slice(1).join(" ");
     console.log("User input topic is: ", topic);
+    console.log("User input age group is: ", ageGroup);
     
-    if (!topic) {
-      message.reply("Please specify a topic. Usage: `/lessonplan [topic]`");
+    if (!topic || !ageGroup) {
+      message.reply("Please specify a topic and an age group. Usage: `/lessonplan [topic] [age group]`");
       return;
     }
     
-    getLessonPlan(topic).then(result => {
+    getLessonPlan(topic, ageGroup).then(result => {
       console.log("Generated lesson plan: ", result);
-      if (result && result.trim() !== '') {
-        if (result.length > 2000) {
-          result = result.substring(0, 1997) + "...";
+      if (result) {
+        let replyMessage = "Here's your lesson plan:\n";
+        for (const [key, value] of Object.entries(result)) {
+          replyMessage += `\n**${key}**:\n${value}\n`;
         }
-        message.reply(`Here's your lesson plan:\n${result}`);
+        
+        if (replyMessage.length > 2000) {
+          replyMessage = replyMessage.substring(0, 1997) + "...";
+        }
+        
+        message.reply(replyMessage);
       }
     }).catch(error => {
       console.error('Error in generateLessonPlan getLessonPlan:', error);
