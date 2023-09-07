46c8cf77eaeed089b5f3e4957ed4d1a596956cf8
kylestocks <kjd3299@gmail.com>
2023-09-06 23:47:36 -0700

    parsed

--- helpers.js
+++ helpers.js
@@ -1,14 +1,8 @@
-import { getAnswer, getLessonPlan } from "./api/openaiApi.js";
+import { getLessonPlan } from "./api/openaiApi.js";
 import PDFDocument from 'pdfkit';
-import fs from 'fs/promises';  // Import promise-based fs
-import fsCore from 'fs';  // Import core fs for createWriteStream
+import fs from 'fs/promises';
+import fsCore from 'fs';
 import { AttachmentBuilder } from 'discord.js';
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export async function generatePDF(lessonPlan, topic, ageGroup) {
   const pdfFileName = `${topic}_${ageGroup}_LessonPlan.pdf`;
@@ -26,8 +20,10 @@ export async function generatePDF(lessonPlan, topic, ageGroup) {
   // Add title
   doc.fontSize(24).text(`${topic} Lesson Plan for Age Group ${ageGroup}`, { align: 'center' }).fontSize(12);
 
+  // Parse and add content
   for (const [key, value] of Object.entries(lessonPlan)) {
-    doc.addPage().fontSize(18).text(key, { underline: true }).fontSize(12).text(value);
+    doc.fontSize(18).text(key, { underline: true }).fontSize(12).text(value);
+    doc.moveDown();
   }
 
   doc.end();
@@ -38,27 +34,24 @@ export async function generatePDF(lessonPlan, topic, ageGroup) {
       writeStream.on('error', reject);
     });
     const pdfBuffer = Buffer.from(pdfChunks);
-    console.log('File has been written');
     return { pdfFileName, pdfBuffer };
   } catch (error) {
-    console.log('An error occurred:', error);
     throw error;
   }
 }
 
-export async function openaiAnswer(message, client) {
+export async function generateAndSendLessonPlan(client, channelId, topic, ageGroup) {
   try {
-    const question = message.content.replace(client.user.id, "").replace("<@> ", "").trim();
-    const result = await getAnswer(question);
-    if (result && result.trim() !== '') {
-      message.reply(result);
-    }
+    const lessonPlanText = await getLessonPlan(topic, ageGroup);
+    const lessonPlan = parseLessonPlan(lessonPlanText);
+    const { pdfFileName, pdfBuffer } = await generatePDF(lessonPlan, topic, ageGroup);
+    await sendPDF(client, channelId, pdfFileName, pdfBuffer);
   } catch (error) {
-    console.error('Error in openaiAnswer:', error);
+    console.error('Error in generateAndSendLessonPlan:', error);
   }
 }
 
-export async function convertToJSON(planText) {
+function parseLessonPlan(planText) {
   const sections = ["Objective", "Materials", "Introduction", "Activities", "Closure", "Extension Activities", "Assessments"];
   const lessonPlan = {};
 
@@ -73,44 +66,15 @@ export async function convertToJSON(planText) {
   return lessonPlan;
 }
 
-export async function sendPDF(client, channelId, pdfFileName) {
+async function sendPDF(client, channelId, pdfFileName, pdfBuffer) {
   try {
-    const pdfPath = path.join(__dirname, pdfFileName);
-
-    // Check if file exists
-    await fs.access(pdfPath);
-
-    // Read the file into a buffer
-    const buffer = await fs.readFile(pdfPath);
-
-    // Create an attachment using AttachmentBuilder
     const attachment = new AttachmentBuilder()
       .setName(pdfFileName)
-      .setFile(buffer);
+      .setFile(pdfBuffer);
 
-    // Send the PDF
     await client.channels.cache.get(channelId).send({ files: [attachment] });
-
-    console.log('Message with PDF sent successfully.');
   } catch (error) {
     console.error('Error sending PDF:', error);
     throw error;
-  }
-}
-
-export async function generateAndSendLessonPlan(client, channelId, topic, ageGroup) {
-  try {
-    // Fetch the lesson plan from OpenAI
-    const lessonPlanText = await getLessonPlan(topic, ageGroup);
-    console.log("Lesson Plan Text:", lessonPlanText);  // Debugging line
-    const lessonPlanJSON = await convertToJSON(lessonPlanText);  // Added await here
-
-    // Generate the PDF
-    const { pdfFileName, pdfBuffer } = await generatePDF(lessonPlanJSON, topic, ageGroup);
-
-    // Send the PDF
-    await sendPDF(client, channelId, pdfFileName, pdfBuffer);
-  } catch (error) {
-    console.error('Error in generateAndSendLessonPlan:', error);
   }
 }
